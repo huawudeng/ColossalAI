f24a657d76354f0cd287eb33773676c06487fed9
huawudeng <80924964+huawudeng@users.noreply.github.com>
2023-03-30 10:15:51 -0400

    Test weights dumping or reloading issue in CachedEmbeddingBag

--- tests/test_layers/test_cache_embedding.py
+++ tests/test_layers/test_cache_embedding.py
@@ -366,6 +366,221 @@ def test_parallel_freq_aware_embed(world_size):
     run_func = partial(run_dist, world_size=world_size, port=free_port())
     mp.spawn(run_func, nprocs=world_size)
 
+def test_cached_embedding_bag_reloading():
+    '''
+    Test scenario: Find out if the model can continue the trend of the loss
+        Step 1: Train the model for a number steps
+        Step 2. Save the model as checkpoint
+        Step 3. Load the model from checkpoint
+        Step 4. Continue training
+    '''
+    from pathlib import Path
+    import tqdm
+
+    class RandomNegativeSampler():
+        def __init__(self, item_probs, pool_size=1000000):
+            self.random = np.random.default_rng(11111)
+            self.item_probs = np.array(item_probs)/sum(item_probs)
+            self.pool_size = pool_size
+            self.all_items = np.arrage(len(item_probs))
+            self.pool = self.random.choice(self.all_items, size=self.pool_size, 
+                            p=self.item_probs, replace=True)
+        
+        def get_sample(self,sample_size):
+            return self.random.choice(self.pool, size=sample_size, relace=True)
+
+    class RecModel(torch.nn.Module):
+        def __init__(self, total_no_items, emb_size, interm_size, output_size, layers, emb_weight=None):
+            super().__init__()
+
+            self.negatives = 20
+
+            item_probs = [0, 0] + np.random.rand(total_no_items-2)
+
+            self.sampler = RandomNegativeSampler(item_probs, total_no_items)
+
+            self.embedding_layer = CachedEmbeddingBag(total_no_items, emb_size, _weight=emb_weight,
+                                        dtype=torch.bfloat16, cache_ratio=0.02)
+            self.mappings = torch.nn.ModuleList([torch.nn.Linear(emb_size, interm_size) 
+                                        for _ in range(layers)])
+
+            self.projection = torch.nn.Linear(emb_size, output_size)
+
+            self.activation = torch.nn.GELU()
+
+            self.loss = torch.nn.CrossEntropyLoss()
+        
+        def mask(self, inputs, prob=0.2):
+            '''Randomly mask some items'''
+
+            labels = inputs.clone()
+            rnd = torch.rand(size=labels.shape, device=labels.device)
+
+            input_mask = (rnd < prob) & (labels != 0)
+            labels = labels * input_mask
+            masked_inputs = 1 * input_mask + inputs * (1-1*input_mask)
+            negatives = self.sampler.get_sample(labels.shape[0] * labels.shape[1] * self.negatives)
+            negatives = negatives.reshape(labels.shape[0], labels.shape[1], self.negatives)
+            labels = torch.cat([labels.unsqueeze(2), negatives], dim=2)
+
+            return masked_inputs, labels
+
+        def forward(self, X):
+            '''X is 2D tensor: batch size x number_items'''
+
+            X, labels = self.mask(X)
+
+            x_2d_shape = X.shape
+            X = X.view(1, -1).squeeze(dim=0) # To 1D
+            offsets = torch.arange(X.shape[0], device=X.device)
+            emb_1d_shape = torch.cat([torch.tensor(x_2d_shape), torch.tensor([-1])])
+
+            embeddings = self.embedding_layer(X, offsets).to(torch.float32).view(torch.size(emb_1d_shape))
+
+            for mapping in self.mappings:
+                embeddings = self.activation(mapping(embeddings))
+            
+            embeddings = self.projection(embeddings)
+
+            x_2d_shape = labels.shape
+            X = labels.view(1, -1).squeeze(dim=0) # To 1D
+            offsets = torch.arange(labels.shape[0], device=labels.device)
+            emb_1d_shape = torch.cat([torch.tensor(x_2d_shape), torch.tensor([-1])])
+
+            label_embeddings = self.embedding_layer(labels, offsets).to(torch.float32).view(torch.size(emb_1d_shape))
+
+            result = torch.einsum("bse, bsne -> bsn", embeddings, label_embeddings)
+
+            ground_truth = torch.zeros(size=result.shape, device=result.device)
+            ground_truth[:,:, 0] = 1
+            loss = self.loss(result.swapaxes(1,2), target=groud_truth.swapaxes(1,2))
+
+            is_masked = (1* (X == 1)).to(X.device)
+            loss = loss * is_masked / is_masked.sum()
+
+            return loss
+
+    class DataGenerator:
+        def __init__(self, total_no_items, items_per_batch, batches):
+            self.total_no_items = total_no_items
+            self.items_per_batch = items_per_batch # Should be multiples of 6
+            self.repeats = self.items_per_batch // 6
+            self.batches = batches
+
+        def _generate_record(self):
+            '''Generate the record on the fly'''
+            record = np.zeros(self.items_per_batch)
+            for i in range(self.repeats):
+                # Generate 5 random numbers
+                start_numbers = np.random.randint(low=2, high=total_no_items, size=5)
+                # Then pick one from one of them with probability
+                prob = np.array([0.05, 0.1, 0.15, 0.2, 0.5])
+                #Perturbate the probability with a noise and normalize them
+                prob += np.random.rand(1) * 0.05
+                prob /= prob.sum()
+
+                pick_number = np.random.choice(start_numbers, p=prob)
+                record[i*6:i*6+5] = start_numbers.copy()
+                record[i*6+5] = pick_number.copy()
+            
+            return record
+        
+        def next(self):
+            batches = []
+            for i in range(self.batches):
+                batches.append(self._generate_record())
+            
+            return torch.tensor(np.stack(batches))
+
+
+    def train(checkpoint_file="", max_batches=100000):
+        '''Training the model for a specific number of batches'''
+
+        # Check if there is an embedding weight stored in checkpoint file
+        cpu_weight = None
+        checkpoint_dict = None
+
+        if checkpoint_file is not None and len(checkpoint_file)>0:
+            if Path(emb_checkpoint_file).exists():
+                checkpoint_dict = torch.load(emb_checkpoint_file, map_location='cpu')
+
+                cpu_weight = checkpoint_dict['cpu_weight']
+        
+        total_no_items, emb_size, interm_size, output_size, layers = 1000000, 128, 512, 128, 5
+
+        # Initiate the model, which should be on GPU because of CachedEmbeddingBag
+        model = RecModel(total_no_items, emb_size, interm_size, output_size, layers,
+                        emb_weight=cpu_weight).to('cuda:0')
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001, wd=0.001)
+        scheduler = torch.optim_lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_batches)
+        
+        # Now reload the rest model parameters if available
+        if checkpoint_dict is not None:
+            model.load_state_dict(checkpoint_dict['model_state_dict'])
+            optimizer.load_state_dict(checkpoint_dict['optimizer_state_dict'])
+            scheduler.load_state_dict(checkpoint_dict['scheduler_state_dict'])
+
+        batch_count = 0
+        batches_for_saving = 100
+        pbar = tqdm.tqdm(total=batches_for_saving, ascii=True)
+
+        items_per_batch, batches = 120, 32
+        train_generator = DataGenerator(total_no_items, items_per_batch, batches)
+
+        while True:
+            model.train()
+            optimizer.zero_grad()
+
+            batch = train_generator.next()
+
+            loss = model(batch)
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            pbar.update(1)
+            pbar.set_postfix(loss=np.mean(loss))
+            batch_count += 1
+
+            if batch_count > 0 and batch_count % batches_for_saving == 0:
+                model_dict = {"model_state_dict": model.state_dict(),
+                              "optimizer_state_dict": optimizer.state_dict(),
+                              "scheduler_state_dict": scheduler.state_dict(),
+                              "cpu_weight": model.embedding_layer.cache_weight_mgr.weight
+                            }
+                
+                torch.save(model_dict, checkpoint_file)
+
+
+            if batch_count >= max_batches:
+                # Last save and exit
+                model_dict = {"model_state_dict": model.state_dict(),
+                              "optimizer_state_dict": optimizer.state_dict(),
+                              "scheduler_state_dict": scheduler.state_dict(),
+                              "cpu_weight": model.embedding_layer.cache_weight_mgr.weight
+                            }
+                
+                torch.save(model_dict, checkpoint_file)
+
+                break
+
+    # Now chain all those together to similate my application scenarios
+
+    # Step 1: Train the model for 10000 steps
+    # Please pay attention to the loss by the end of the training
+    checkpoint_file = "./my_model_data.chkp"
+    train(checkpoint_file=checkpoint_file, max_batches=10000)
+
+    # Step 2: Now reload the trained model from the checkpoint file and resume
+    # training for 100000
+    # Please pay attention to the loss at the beginning of the training
+    # It should be similar to the loss of Step 1 by the endo the training
+    # However, that is not the case: the loss is much higher than that.
+    # My best guess is the cpu_weight is not probably propagated to cuda_cached_weight
+    # or the cpu_weight does not capture the trained by the end of Step 1
+    train(checkpoint_file=checkpoint_file, max_batches=10000)
+
+
 
 if __name__ == '__main__':
     # test_freq_aware_embed(True)
